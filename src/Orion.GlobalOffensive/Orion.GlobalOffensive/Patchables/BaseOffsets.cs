8fc1e70269283b7e83536ce8c94f31ffba9d02f5
aevitas <m4rtijn@gmail.com>
2015-06-16 20:50:37 +0200

    Updated offsets

--- src/Orion.GlobalOffensive/Orion.GlobalOffensive/Patchables/BaseOffsets.cs
+++ src/Orion.GlobalOffensive/Orion.GlobalOffensive/Patchables/BaseOffsets.cs
@@ -6,9 +6,9 @@ namespace Orion.GlobalOffensive.Patchables
 	// Offsets that change at least once per build, and are platform specific.
 	public enum BaseOffsets
 	{
-		ViewMatrix = 0x4a0d164,
-		EntityList = 0x4a17bd4,
+		ViewMatrix = 0x4a0d1a4,
+		EntityList = 0x4a17c14,
 		EnginePtr = 0x5ce294,
-		LocalPlayer = 0xa75ccc
+		LocalPlayer = 0xa75cdc
 	}
 }
